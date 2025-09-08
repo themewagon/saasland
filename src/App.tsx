5579cca626f8ed14665281b97928cc4ef14d9837
KhanAnupamShafi <deep71.bd@gmail.com>
2025-09-08 23:29:50 +0600

    browser router updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Toaster } from '@/components/ui/toaster';
-import { Toaster as Sonner } from '@/components/ui/sonner';
-import { TooltipProvider } from '@/components/ui/tooltip';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Index from './pages/Index';
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
@@ -12,16 +12,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/saasland">
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/" />
-          <Route path="/" />
-          <Route path="/" />
-          <Route path="/" />
-          <Route path="/" />
-          <Route path="/" />
-          <Route path="/" />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
